5549291b079252bcd2ff0a2bc5a7c78f2e3a678b
acamm <aijun.camm@gmail.com>
2019-02-28 14:48:13 -0400

    Populate items in order after reading from DB so can check for unsold

--- src/main/java/Nuskin/OrderController.java
+++ src/main/java/Nuskin/OrderController.java
@@ -16,6 +16,13 @@ public class OrderController {
 	public Iterable<Order> getOrders() {
 	    
 		Iterable<Order> orders = orderRepo.findAll();
+		
+		// Products on the order are marked transient, not loaded from the database
+		// Need to get them now so that we can work out if any items still dont have an end user
+		for (Order order: orders) {
+			order.getProductsFromDatabase();
+		}
+		
 		//List<Order> target = new ArrayList<>();
 		//orders.forEach(target::add);  // :: syntax is a method reference
 		//orders.forEach( n-> {target.add(n); } );
