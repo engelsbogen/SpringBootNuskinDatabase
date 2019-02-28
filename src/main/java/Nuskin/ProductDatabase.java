d07e9c2eea1f420a5068bb69011bc1d8380449e0
acamm <aijun.camm@gmail.com>
2019-02-28 14:41:47 -0400

    Schedule database backup every day at midnight

--- src/main/java/Nuskin/ProductDatabase.java
+++ src/main/java/Nuskin/ProductDatabase.java
@@ -1,14 +1,26 @@
 package Nuskin;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductDatabase {
 
-	private static ProductDatabase theProductDatabase;
+	private Logger  log =  LoggerFactory.getLogger(ProductDatabase.class);
+	
+	private static ProductDatabase theProductDatabase = null;
 	
 	@Autowired
     private  ProductRepository productRepository;
@@ -25,7 +37,6 @@ public class ProductDatabase {
 		theProductDatabase = this;
 	}
 	
-
 	void addProductType(ProductType productType) {
 		productTypeRepository.save(productType);
 	}
@@ -38,6 +49,18 @@ public class ProductDatabase {
 		orderRepository.save(order);
 	}
 	
+	Order getOrder(String orderNumber) {
+		Optional<Order> optOrder = orderRepository.findById(orderNumber);
+		
+		Order order = null;
+		if (optOrder.isPresent()) {
+			order = optOrder.get();
+			order.getProductsFromDatabase();
+		}
+		
+		return order;
+	}
+	
 	ProductType findProductType(String SKU) {
 		Optional<ProductType> productType = productTypeRepository.findBySku(SKU);
 		
@@ -48,4 +71,84 @@ public class ProductDatabase {
 		return productType.get();
 	}
 	
+	ArrayList<Product> getOrderItems(String orderNumber) { 
+		return productRepository.findAllByOrderOrderNumber(orderNumber);
+	}
+	
+    @Scheduled(cron = "0 0 0 * * *")  // Backup at midnight every day
+	public boolean backup() {
+		
+		String user="adc";
+		String password="letmein";
+		String database="Nuskin";
+		String backupPath = FileRoot.getRoot() + "Backups";
+		
+		return doBackup(user,password,database,backupPath);
+	}
+		
+	private boolean doBackup(String user, String password, String database, String backupPath) {
+		
+        boolean status = false;
+        
+        try {
+            DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+            Date date = new Date();
+            String backupFilename = "backup-" + database + "-" + dateFormat.format(date) + ".sql";
+            
+            ProcessBuilder pb = new ProcessBuilder("mysqldump", 
+            		                               "-u", user,
+            		                               "--password=" + password,
+            		                               "--add-drop-database",
+            		                               "-B",
+            		                               database,
+            		                               "-r", backupPath + "/" + backupFilename);
+            
+            Process p = pb.start();
+          
+            int processStatus = p.waitFor();
+ 
+            if (processStatus == 0) {
+                status = true;
+                log.info("Backup created successfully for " + database );
+            } else {
+                status = false;
+                log.info("Could not create the backup for " + database );
+            }
+            
+ 
+        } catch (IOException ioe) {
+            log.error("IOException:", ioe, ioe.getCause());
+        } catch (Exception e) {
+            log.error("Exception:", e, e.getCause());
+        }
+        return status;
+	}
+	
+	
+	
+	
+		
+	public boolean restore(String dbUserName, String dbPassword, String source) {
+	 
+		// Source SQL text contains the database name so. Maybe should check this is Nuskin before doing anything?
+        ProcessBuilder pb = new ProcessBuilder("mysql", "--user=" + dbUserName, "--password=" + dbPassword, "-e", "source " + source);
+ 
+        Process runtimeProcess;
+        try {
+            runtimeProcess = pb.start();
+            int processComplete = runtimeProcess.waitFor();
+ 
+            if (processComplete == 0) {
+                log.info("Backup restored successfully with " + source);
+                return true;
+            } else {
+                log.info("Could not restore the backup " + source);
+            }
+        } catch (Exception ex) {
+            log.error("Exception: " + ex, ex.getCause());
+        }
+ 
+        return false;
+ 
+    }
 }
