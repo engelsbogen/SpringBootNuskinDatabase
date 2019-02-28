450a6f96e59ea9305740c16bba730f25c0f6e1cc
acamm <aijun.camm@gmail.com>
2019-02-28 14:51:49 -0400

    Added 'updateitems' methods to allow editing of customer etc

--- src/main/java/Nuskin/ProductController.java
+++ src/main/java/Nuskin/ProductController.java
@@ -1,7 +1,13 @@
 package Nuskin;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +27,40 @@ public class ProductController {
 		ArrayList<Product> products = productRepo.findAllByOrderOrderNumber(orderNumber);
 		return products;
     }
+	
+    @PutMapping("/updateitems")
+    public ResponseEntity<?>  updateItems(@RequestBody ArrayList<Product> updatedItems) {
+    	
+    	
+    	// We only want to update EndUse, Customer, Selling Price and Receipt number
+    	
+    	// So - fetch the current, overwrite those fields, write it back?
+    	for (Product updatedItem : updatedItems) {
+    		
+    		Optional<Product> existingItemOpt = productRepo.findById(updatedItem.getId());
+    		
+    		if (existingItemOpt.isPresent()) {
+    			
+    			Product existingItem = existingItemOpt.get();
+    			existingItem.setReceiptNumber(updatedItem.getReceiptNumber());
+    			existingItem.setEndUse(updatedItem.getEndUse());
+    			existingItem.setSellingPrice(updatedItem.getSellingPrice());
+    			existingItem.setCustomerName(updatedItem.getCustomerName());
+    			
+    			// This actually creates or updates - but as I know its an existing item it just updates
+    			productRepo.save(existingItem);
+    			
+    		}
+
+    	}
+    	
+		// Flush the changes  
+		productRepo.flush();
+		
+		System.out.println("Flushed database changes");
+		
+        return ResponseEntity.ok().build();
+    }
+
 	    
 }
