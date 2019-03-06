84f22745cae811361d084579d1e5ffc8c257add5
acamm <aijun.camm@gmail.com>
2019-03-06 17:56:54 -0400

    Backup database to external USB and google drive

--- src/main/java/Nuskin/FileRoot.java
+++ src/main/java/Nuskin/FileRoot.java
@@ -1,9 +1,35 @@
 package Nuskin;
 
+import java.io.File;
+
 public class FileRoot {
 
+	
+	public static String getSecondaryPath() {
+		
+		return "/media/WDMyBook/Nuskin/";
+		
+	}
+	
 	public static String getRoot() {
-		return "/home/adc/eclipse-angular/SpringBootNuskinDatabase/TextFiles/";
+
+		
+		String[] locations = { 
+			"/home/adc/eclipse-angular/SpringBootNuskinDatabase/TextFiles/",
+			"/home/adc/nuskin-order-manager/"	
+		};
+		
+		for (String s: locations) {
+
+			File path = new File(s);
+			
+			if (path.exists()) {
+				return s;
+			}
+		}
+
+		return "/";
+		
 	}
 	
 }
