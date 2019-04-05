4c55b80c06055c47892d46891e714c3d35771422
acamm <aijun.camm@gmail.com>
2019-04-05 16:42:58 -0300

    Remove unused imports

--- src/main/java/Nuskin/AccountRepository.java
+++ src/main/java/Nuskin/AccountRepository.java
@@ -1,9 +1,6 @@
 package Nuskin;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
 public interface AccountRepository extends JpaRepository<Account, String> {
 
